078b39162c4bbac4827ce54678fc26c1a3b09202
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-05-05 15:52:14 -0400

    Add Customer API endpoint

---
+++ src/WebApp/Api/CustomerApiController.cs
@@ -0,0 +1,29 @@
+﻿using GaEpd.AppLibrary.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using Sbeap.AppServices.Customers;
+using Sbeap.AppServices.Customers.Dto;
+
+namespace MyAppRoot.WebApp.Api;
+
+[ApiController]
+[Route("api/customers")]
+[Produces("application/json")]
+public class CustomerApiController : Controller
+{
+    private readonly ICustomerService _service;
+    public CustomerApiController(ICustomerService service) => _service = service;
+
+    [HttpGet]
+    public async Task<IPaginatedResult<CustomerSearchResultDto>> ListCustomersAsync(
+        [FromQuery] CustomerSearchDto spec,
+        [FromQuery] ushort page = 1,
+        [FromQuery] ushort pageSize = 25) =>
+        await _service.SearchAsync(spec, new PaginatedRequest(page, pageSize));
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<CustomerViewDto>> GetCustomerAsync([FromRoute] Guid id)
+    {
+        var item = await _service.FindAsync(id);
+        return item is null ? Problem("ID not found.", statusCode: 404) : Ok(item);
+    }
+}
