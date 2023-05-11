1551be2c0002feb7d32c99ee5721d90163bf8bd3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-05-11 07:47:24 -0400

    Add Admin User role

---
+++ src/AppServices/Customers/Permissions/CustomerViewPermissionsHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Sbeap.AppServices.Customers.Dto;
+using Sbeap.AppServices.Customers.Permissions;
+using Sbeap.Domain.Identity;
+using System.Security.Principal;
+
+namespace Cts.AppServices.Complaints.Permissions;
+
+internal class CustomerViewPermissionsHandler :
+    AuthorizationHandler<CustomerOperation, CustomerViewDto>
+{
+    private static bool IsAdminUser(IPrincipal user) =>
+        user.IsInRole(RoleName.Admin);
+
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        CustomerOperation requirement,
+        CustomerViewDto resource)
+    {
+        if (!(context.User.Identity?.IsAuthenticated ?? false))
+            return Task.FromResult(0);
+
+        var success = requirement.Name switch
+        {
+            CustomerOperationNames.ManageDeletions =>
+                // Only an Admin User can delete or restore.
+                IsAdminUser(context.User),
+
+            _ => throw new ArgumentOutOfRangeException(nameof(requirement)),
+        };
+
+        if (success) context.Succeed(requirement);
+        return Task.FromResult(0);
+    }
+}
--- src/AppServices/Permissions/Requirements/AdminUserRequirement.cs
+++ src/AppServices/Permissions/Requirements/AdminUserRequirement.cs
@@ -3,17 +3,17 @@ using Sbeap.Domain.Identity;
 
 namespace Sbeap.AppServices.Permissions.Requirements;
 
-internal class StaffUserRequirement :
-    AuthorizationHandler<StaffUserRequirement>, IAuthorizationRequirement
+internal class AdminUserRequirement :
+    AuthorizationHandler<AdminUserRequirement>, IAuthorizationRequirement
 {
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
-        StaffUserRequirement requirement)
+        AdminUserRequirement requirement)
     {
         if (!(context.User.Identity?.IsAuthenticated ?? false))
             return Task.FromResult(0);
 
-        if (context.User.IsInRole(RoleName.Staff) || context.User.IsInRole(RoleName.Admin))
+        if (context.User.IsInRole(RoleName.Admin))
             context.Succeed(requirement);
 
         return Task.FromResult(0);
