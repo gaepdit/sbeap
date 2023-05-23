be611f0fb2ac1f5217a2e286d33b282df1e60f4c
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2023-05-23 17:11:13 -0400

    Add user dashboard to home page (#94)

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using GaEpd.AppLibrary.Enums;
+using GaEpd.AppLibrary.Pagination;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sbeap.AppServices.Cases;
+using Sbeap.AppServices.Cases.Dto;
+using Sbeap.Domain.Identity;
+using Sbeap.WebApp.Platform.Constants;
 
 namespace Sbeap.WebApp.Pages;
 
 [AllowAnonymous]
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    public IndexModel(ILogger<IndexModel> logger) => _logger = logger;
+    // Constructor
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly ICaseworkService _cases;
 
-    public void OnGet()
+    public IndexModel(SignInManager<ApplicationUser> signInManager, ICaseworkService cases)
     {
-        // Method intentionally left empty.
+        _signInManager = signInManager;
+        _cases = cases;
+    }
+
+    // Properties
+    public bool ShowDashboard { get; private set; }
+    public IPaginatedResult<CaseworkSearchResultDto> OpenCases { get; private set; } = default!;
+
+    // Methods
+    public async Task<IActionResult> OnGetAsync()
+    {
+        if (!_signInManager.IsSignedIn(User)) return LocalRedirect("~/Account/Login");
+        if (!User.IsInRole(RoleName.Staff) && !User.IsInRole(RoleName.Admin)) return Page();
+
+        var openCasesSpec = new CaseworkSearchDto { Status = CaseStatus.Open, Sort = CaseworkSortBy.OpenedDate };
+        var paging = new PaginatedRequest(1, GlobalConstants.PageSize, openCasesSpec.Sort.GetDescription());
+        OpenCases = await _cases.SearchAsync(openCasesSpec, paging);
+
+        ShowDashboard = true;
+        return Page();
     }
 }
