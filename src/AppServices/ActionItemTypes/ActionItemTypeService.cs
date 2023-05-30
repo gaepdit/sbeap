96617c72ab65b76e818838d884015bc9a246f11e
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-05-30 14:27:55 -0400

    Add ActionItemType Service WIP

---
+++ src/AppServices/ActionItemTypes/ActionItemTypeService.cs
@@ -0,0 +1,48 @@
+﻿using GaEpd.AppLibrary.ListItems;
+using Sbeap.AppServices.ActionItemTypes.Dto;
+using Sbeap.Domain.Entities.ActionItemTypes;
+using Sbeap.Domain.Entities.Agencies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sbeap.AppServices.ActionItemTypes;
+
+public sealed class ActionItemTypeService : IActionItemTypeService
+{
+    private readonly IActionItemTypeRepository _repository;
+    
+    public ActionItemTypeService(IActionItemTypeRepository repository) => _repository = repository;
+
+    public Task AddActionItemTypeAsync(ActionItemTypeCreateDto resource, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteActionItemTypeAsync(Guid actionItemId, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionItemTypeUpdateDto?> FindActionItemTypeForUpdateAsync(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<ListItem>> GetListItemsAsync(bool activeOnly = true, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateActionItemTypeAsync(ActionItemTypeUpdateDto resource, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
