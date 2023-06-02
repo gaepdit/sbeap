7e2b4d13b18e6569b5b7109860bb16fb3c93a24c
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-06-02 19:16:32 -0400

    App Services WIP for ActionItemTypes
    Todo:Testing.  Errors on build with validators

--- src/AppServices/ActionItemTypes/Permissions/ActionItemTypeViewPermissionsHandler.cs
+++ src/AppServices/ActionItemTypes/Permissions/ActionItemTypeViewPermissionsHandler.cs
@@ -7,14 +7,32 @@ namespace Sbeap.AppServices.ActionItemTypes.Permissions;
 internal class ActionItemTypeViewPermissionsHandler :
     AuthorizationHandler<ActionItemTypeOperation, ActionItemTypeViewDto>
 {
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ActionItemTypeOperation requirement, ActionItemTypeViewDto resource)
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context, 
+        ActionItemTypeOperation requirement, 
+        ActionItemTypeViewDto resource)
     {
-        throw new NotImplementedException();
+        if (!(context.User.Identity?.IsAuthenticated ?? false))
+            return Task.FromResult(0);
+
+        // Differentiating Editing and Delete/Restore even though both are presently Admin only
+        // in case staff users will need rights to these in the future
+        bool success = requirement.Name switch
+        {
+            nameof(ActionItemTypeOperation.Edit) =>
+                // Only an Admin User can edit.
+                IsAdminUser(context.User),
+
+            nameof(ActionItemTypeOperation.ManageDeletions) =>
+                // Only an Admin User can delete or restore.
+                IsAdminUser(context.User),
+
+            _ => throw new ArgumentOutOfRangeException(nameof(requirement)),
+        };
+
+        if (success) context.Succeed(requirement);
+        return Task.FromResult(0);
     }
 
     private static bool IsAdminUser(IPrincipal user) => user.IsInRole(RoleName.Admin);
-
-    private static bool IsStaffUser(IPrincipal user) => user.IsInRole(RoleName.Staff) || user.IsInRole(RoleName.Admin);
-
-    private static bool IsNotDeleted(ActionItemTypeViewDto resource) => resource.Active;
 }
