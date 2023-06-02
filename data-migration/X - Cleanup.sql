df9064b793cf7e1f9101d3073775835c71715071
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-06-02 14:46:10 -0400

    Add data migration for Contacts

--- data-migration/X - Cleanup.sql
+++ data-migration/X - Cleanup.sql
@@ -2,6 +2,14 @@ use [sbeap-app]
 
 -- Remove temporary columns
 
+alter table Contacts
+    drop column AirBranchContactId
+go
+
+alter table Contacts
+    drop column AirBranchCustomerId
+go
+
 alter table Customers
     drop column AirBranchCustomerId
 go
