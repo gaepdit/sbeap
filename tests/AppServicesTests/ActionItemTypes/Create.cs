2ce0fc55375ba376b37a77fb04f4755fde5e5de1
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-06-05 15:08:55 -0400

    Implement Tests for ActionItemType AppServices WIP

--- tests/AppServicesTests/ActionItemTypes/Create.cs
+++ tests/AppServicesTests/ActionItemTypes/Create.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sbeap.AppServices.ActionItemTypes;
+using Sbeap.AppServices.UserServices;
+using Sbeap.Domain.Entities.ActionItemTypes;
+using Sbeap.Domain.Identity;
+using Sbeap.TestData.Constants;
 
-namespace AppServicesTests.ActionItemTypes
+namespace AppServicesTests.ActionItemTypes;
+
+public class Create
 {
-    internal class Create
+    [Test]
+    public async Task WhenResourceIsValid_ReturnsId()
     {
+        var item = new ActionItemType(Guid.NewGuid(), TextData.ValidName);
+        var repoMock = new Mock<IActionItemTypeRepository>();
+        var managerMock = new Mock<IActionItemTypeManager>();
+        managerMock.Setup(l =>
+                l.CreateAsync(It.IsAny<string>(), null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(item);
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(l => l.GetCurrentUserAsync())
+            .ReturnsAsync((ApplicationUser?)null);
+        var appService = new ActionItemTypeService(repoMock.Object, managerMock.Object,
+            AppServicesTestsSetup.Mapper!, userServiceMock.Object);
+        var resource = new ActionItemTypeCreateDto(TextData.ValidName);
+
+        var result = await appService.CreateActionItemTypeAsync(resource);
+
+        result.Should().Be(item.Id);
     }
 }
