d4c2a55595fb79dd1d6a8665f71c987fe8042bd0
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-06-30 17:40:20 -0400

    Create ActionItemType EF Repository

--- src/EfRepository/Repositories/ActionItemTypeRepository.cs
+++ src/EfRepository/Repositories/ActionItemTypeRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sbeap.Domain.Entities.ActionItemTypes;
+using Sbeap.EfRepository.Contexts;
+using Microsoft.EntityFrameworkCore;
 
-namespace Sbeap.EfRepository.Repositories
+namespace Sbeap.EfRepository.Repositories;
+
+public sealed class ActionItemTypeRepository : BaseRepository<ActionItemType, Guid>, IActionItemTypeRepository
 {
-    internal class ActionItemTypeRepository
-    {
-    }
+    public ActionItemTypeRepository(AppDbContext context) : base(context) { }
+
+    public async Task<ActionItemType?> FindByNameAsync(string name, CancellationToken token = default) =>
+        await Context.ActionItemTypes.AsNoTracking()
+        .SingleOrDefaultAsync(e => string.Equals(e.Name.ToUpper(), name.ToUpper()), cancellationToken: token);
 }
