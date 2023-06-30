3e18b732fc0c728eb6b0138e96cf79de3663fbd5
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-06-30 18:38:30 -0400

    ActionItemType Controller

---
+++ src/WebApp/Api/ActionItemTypeApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sbeap.AppServices.ActionItemTypes;
+using Sbeap.AppServices.Offices;
+
+namespace Sbeap.WebApp.Api;
+
+[ApiController]
+[Route("api/ActionItemTypes")]
+[Produces("application/json")]
+public class ActionItemTypeApiController : Controller
+{
+    private readonly IActionItemTypeService _actionItemTypesService;
+    public ActionItemTypeApiController(IActionItemTypeService actionItemTypeService) => _actionItemTypesService = actionItemTypeService;
+
+    [HttpGet]
+    public async Task<IReadOnlyList<ActionItemTypeViewDto>> ListActionItemTypesServiceAsync() =>
+        (IReadOnlyList<ActionItemTypeViewDto>)await _actionItemTypesService.GetListItemsAsync();
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ActionItemTypeViewDto>> GetActionItemTypeAsync([FromRoute] Guid id)
+    {
+        var item = await _actionItemTypesService.FindActionItemTypeForUpdateAsync(id);
+        return item != null ? Ok(item) : Problem("ID not found.", statusCode: 404);
+    }
+}
