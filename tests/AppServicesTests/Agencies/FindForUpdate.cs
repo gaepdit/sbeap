62fb1fc141109f982737e047ab33b0ff4f1cc789
Tom Karasch <tom.karasch@dnr.ga.gov>
2023-07-07 12:09:57 -0400

    Implement Agency App Services Unit Tests

---
+++ tests/AppServicesTests/Agencies/FindForUpdate.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Sbeap.AppServices.Agencies;
+using Sbeap.AppServices.UserServices;
+using Sbeap.Domain.Entities.Agencies;
+using Sbeap.TestData.Constants;
+
+namespace AppServicesTests.Agencies;
+
+public class FindForUpdate
+{
+    [Test]
+    public async Task WhenItemExists_ReturnsViewDto()
+    {
+        var agency = new Agency(Guid.Empty, TextData.ValidName);
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindAsync(agency.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(agency);
+        var managerMock = new Mock<IAgencyManager>();
+        var userServiceMock = new Mock<IUserService>();
+        var appService = new AgencyService(repoMock.Object, managerMock.Object,
+            AppServicesTestsSetup.Mapper!, userServiceMock.Object);
+
+        var result = await appService.FindAgencyForUpdateAsync(Guid.Empty);
+
+        result.Should().BeEquivalentTo(agency);
+    }
+
+    [Test]
+    public async Task WhenDoesNotExist_ReturnsNull()
+    {
+        var id = Guid.Empty;
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindAsync(id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Agency?)null);
+        var managerMock = new Mock<IAgencyManager>();
+        var mapperMock = new Mock<IMapper>();
+        var userServiceMock = new Mock<IUserService>();
+        var appService = new AgencyService(repoMock.Object, managerMock.Object,
+            mapperMock.Object, userServiceMock.Object);
+
+        var result = await appService.FindAgencyForUpdateAsync(Guid.Empty);
+
+        result.Should().BeNull();
+    }
+}
---
+++ tests/AppServicesTests/Agencies/UpdateValidator.cs
@@ -0,0 +1,67 @@
+﻿using Sbeap.TestData.Constants;
+using Sbeap.Domain.Entities.Agencies;
+using Sbeap.AppServices.Agencies;
+using Sbeap.AppServices.Agencies.Validators;
+using FluentValidation.TestHelper;
+
+namespace AppServicesTests.Agencies;
+
+internal class UpdateValidator
+{
+    [Test]
+    public async Task ValidDto_ReturnsAsValid()
+    {
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Agency?)null);
+        var model = new AgencyUpdateDto(Id: Guid.Empty, Name: TextData.ValidName, Active: true);
+
+        var validator = new AgencyUpdateValidator(repoMock.Object);
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Test]
+    public async Task DuplicateName_ReturnsAsInvalid()
+    {
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Agency(Guid.NewGuid(), TextData.ValidName));
+        var model = new AgencyUpdateDto(Id: Guid.Empty, Name: TextData.ValidName, Active: true);
+
+        var validator = new AgencyUpdateValidator(repoMock.Object);
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldHaveValidationErrorFor(e => e.Name)
+            .WithErrorMessage("The name entered already exists.");
+    }
+
+    [Test]
+    public async Task DuplicateName_ForSameId_ReturnsAsValid()
+    {
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Agency(Guid.Empty, TextData.ValidName));
+        var model = new AgencyUpdateDto(Id: Guid.Empty, Name: TextData.ValidName, Active: true);
+
+        var validator = new AgencyUpdateValidator(repoMock.Object);
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Test]
+    public async Task NameTooShort_ReturnsAsInvalid()
+    {
+        var repoMock = new Mock<IAgencyRepository>();
+        repoMock.Setup(l => l.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Agency?)null);
+        var model = new AgencyUpdateDto(Id: Guid.Empty, Name: TextData.ShortName, Active: true);
+
+        var validator = new AgencyUpdateValidator(repoMock.Object);
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldHaveValidationErrorFor(e => e.Name);
+    }
+}
