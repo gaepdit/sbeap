558862c5e7f15730c6e101a094e857bbe16f143f
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-07-27 14:38:09 -0400

    Fix AnchorJS initialization

---
+++ src/WebApp/wwwroot/js/anchorInit.js
@@ -0,0 +1,9 @@
+﻿// Write your JavaScript code.
+
+const anchors = new AnchorJS();
+// DOMContentLoaded was tested to be the best place to call anchors.add()
+window.addEventListener('DOMContentLoaded', function () {
+    anchors.options.placement = "left";
+    // Add anchors to h2, h3, h4, h5, and h6 elements, but exclude any with the "no-anchor" class.
+    anchors.add('h2:not(.no-anchor), h3:not(.no-anchor), h4:not(.no-anchor), h5:not(.no-anchor), h6:not(.no-anchor)');
+})
--- src/WebApp/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Write your JavaScript code.
-
-const anchors = new AnchorJS();
-// DOMContentLoaded was tested to be the best place to call anchors.add()
-window.addEventListener('DOMContentLoaded', function (event) {
-    anchors.options.placement = "left";
-    // default to adding h2, h3, h4, h5, and h6
-    anchors.add();
-})
