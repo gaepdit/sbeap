af2f8432c60149d762345128c01487303dab6dcd
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-08-09 12:51:14 -0400

    Fix customer edit JS scripts

--- src/WebApp/wwwroot/js/copyAddress.js
+++ src/WebApp/wwwroot/js/copyAddress.js
@@ -1,18 +1,7 @@
-﻿// Copy site location address to mailing address
-const copySiteAddressButton = document.getElementById("copy-site-location");
+﻿// Copy site mailing address to contact address
+const copyMailingAddressButton = document.getElementById("copy-site-address");
 
-copySiteAddressButton.addEventListener("click", () => {
-    document.getElementById("Item_MailingAddress_Street").value = document.getElementById("Item_Location_Street").value;
-    document.getElementById("Item_MailingAddress_Street2").value = document.getElementById("Item_Location_Street2").value;
-    document.getElementById("Item_MailingAddress_City").value = document.getElementById("Item_Location_City").value;
-    document.getElementById("Item_MailingAddress_State").value = document.getElementById("Item_Location_State").value;
-    document.getElementById("Item_MailingAddress_PostalCode").value = document.getElementById("Item_Location_PostalCode").value;
-});
-
-// Copy site mailing address to contact address
-const copyMailingAddress = document.getElementById("copy-site-address");
-
-copyMailingAddress.addEventListener("click", () => {
+copyMailingAddressButton.addEventListener("click", () => {
     document.getElementById("Item_Contact_Address_Street").value = document.getElementById("Item_MailingAddress_Street").value;
     document.getElementById("Item_Contact_Address_Street2").value = document.getElementById("Item_MailingAddress_Street2").value;
     document.getElementById("Item_Contact_Address_City").value = document.getElementById("Item_MailingAddress_City").value;
---
+++ src/WebApp/wwwroot/js/copyLocation.js
@@ -0,0 +1,10 @@
+﻿// Copy site location address to mailing address
+const copySiteAddressButton = document.getElementById("copy-site-location");
+
+copySiteAddressButton.addEventListener("click", () => {
+    document.getElementById("Item_MailingAddress_Street").value = document.getElementById("Item_Location_Street").value;
+    document.getElementById("Item_MailingAddress_Street2").value = document.getElementById("Item_Location_Street2").value;
+    document.getElementById("Item_MailingAddress_City").value = document.getElementById("Item_Location_City").value;
+    document.getElementById("Item_MailingAddress_State").value = document.getElementById("Item_Location_State").value;
+    document.getElementById("Item_MailingAddress_PostalCode").value = document.getElementById("Item_Location_PostalCode").value;
+});
