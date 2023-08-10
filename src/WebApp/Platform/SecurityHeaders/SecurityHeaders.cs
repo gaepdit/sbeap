53bd58f01c4985d3f33f55cb1a9582bf869bbdf7
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2023-08-10 08:40:37 -0400

    Add security HTTP headers (#147)
    * Update NuGet packages
    
    * Add and configure SecurityHeaders package

---
+++ src/WebApp/Platform/SecurityHeaders/SecurityHeaders.cs
@@ -0,0 +1,20 @@
+﻿using Sbeap.WebApp.Platform.Settings;
+
+namespace Sbeap.WebApp.Platform.SecurityHeaders;
+
+internal static class SecurityHeaders
+{
+    internal static void AddSecurityHeaderPolicies(this HeaderPolicyCollection policies)
+    {
+        policies
+            .AddFrameOptionsDeny()
+            .AddXssProtectionBlock()
+            .AddContentTypeOptionsNoSniff()
+            .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+            .RemoveServerHeader();
+
+        if (!string.IsNullOrEmpty(ApplicationSettings.RaygunSettings.ApiKey))
+            policies.AddReportingEndpoints(builder => builder.AddEndpoint("csp-endpoint",
+                $"https://report-to-api.raygun.com/reports-csp?apikey={ApplicationSettings.RaygunSettings.ApiKey}"));
+    }
+}
