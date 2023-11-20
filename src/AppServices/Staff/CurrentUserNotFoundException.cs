928516ab5fb7f7be8924c20f3b39fc5ef200a41b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-11-20 16:02:45 -0500

    Remove serializable attribute

--- src/AppServices/Staff/CurrentUserNotFoundException.cs
+++ src/AppServices/Staff/CurrentUserNotFoundException.cs
@@ -1,16 +1,6 @@
-﻿using System.Runtime.Serialization;
-
-namespace Sbeap.AppServices.Staff;
+﻿namespace Sbeap.AppServices.Staff;
 
 /// <summary>
 /// The exception that is thrown if the current user can't be found.
 /// </summary>
-[Serializable]
-public class CurrentUserNotFoundException : Exception
-{
-    public CurrentUserNotFoundException()
-        : base("Information on the current user could not be found.") { }
-
-    protected CurrentUserNotFoundException(SerializationInfo info, StreamingContext context)
-        : base(info, context) { }
-}
+public class CurrentUserNotFoundException() : Exception("Information on the current user could not be found.");
