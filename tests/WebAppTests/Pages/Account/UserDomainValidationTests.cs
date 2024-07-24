22db8078af02545d720acf778a98d7c7d7658d19
Tanvir Patwary <tanvir.patwary@dnr.ga.gov>
2024-07-24 14:07:58 -0400

    User Tests Sbeap

---
+++ tests/WebAppTests/Pages/Account/UserDomainValidationTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using Sbeap.Domain.Identity;
+
+namespace Sbeap.Domain.Tests.Identity
+{
+    public class UserDomainValidationTests
+    {
+        [Test]
+        public void IsValidEmailDomain_ValidEmail_ReturnsTrue()
+        {
+            var email = "test@dnr.ga.gov";
+
+            var result = email.IsValidEmailDomain();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsValidEmailDomain_InvalidEmail_ReturnsFalse()
+        {
+            var email = "test@invalid.com";
+
+            var result = email.IsValidEmailDomain();
+
+            result.Should().BeFalse();
+        }
+    }
+}
