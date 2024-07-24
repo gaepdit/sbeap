0d65c41e1e25127f916bc8d85851e45bc1656562
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-07-24 16:24:01 -0400

    Fix code style

--- tests/WebAppTests/Pages/Account/UserDomainValidationTests.cs
+++ tests/WebAppTests/Pages/Account/UserDomainValidationTests.cs
@@ -1,28 +1,26 @@
-using NUnit.Framework;
 using Sbeap.Domain.Identity;
 
-namespace Sbeap.WebAppTests.Pages.Account
+namespace WebAppTests.Pages.Account;
+
+public class UserDomainValidationTests
 {
-    public class UserDomainValidationTests
+    [Test]
+    public void IsValidEmailDomain_ValidEmail_ReturnsTrue()
     {
-        [Test]
-        public void IsValidEmailDomain_ValidEmail_ReturnsTrue()
-        {
-            var email = "test@dnr.ga.gov";
+        const string email = "test@dnr.ga.gov";
 
-            var result = email.IsValidEmailDomain();
+        var result = email.IsValidEmailDomain();
 
-            result.Should().BeTrue();
-        }
+        result.Should().BeTrue();
+    }
 
-        [Test]
-        public void IsValidEmailDomain_InvalidEmail_ReturnsFalse()
-        {
-            var email = "test@invalid.com";
+    [Test]
+    public void IsValidEmailDomain_InvalidEmail_ReturnsFalse()
+    {
+        const string email = "test@invalid.com";
 
-            var result = email.IsValidEmailDomain();
+        var result = email.IsValidEmailDomain();
 
-            result.Should().BeFalse();
-        }
+        result.Should().BeFalse();
     }
 }
