03db884459a5caae57faf74ecfad613c2d440c10
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-11-08 13:33:51 -0500

    Add EF Migration

--- src/EfRepository/Migrations/20241108183309_AddAccountAuditingDates.Designer.cs
+++ src/EfRepository/Migrations/20241108183309_AddAccountAuditingDates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sbeap.EfRepository.Contexts;
 
@@ -11,9 +12,11 @@ using Sbeap.EfRepository.Contexts;
 namespace Sbeap.EfRepository.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241108183309_AddAccountAuditingDates")]
+    partial class AddAccountAuditingDates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
