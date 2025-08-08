5962f219b345dc1214569932dba2bc84bd265ecd
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-08-08 09:03:11 -0400

    Fix spelling in comments

--- src/Domain/Entities/SicCodes/ISicRepository.cs
+++ src/Domain/Entities/SicCodes/ISicRepository.cs
@@ -5,7 +5,7 @@ public interface ISicRepository : IDisposable, IAsyncDisposable
     /// <summary>
     /// Determines whether the <see cref="SicCode"/> with the given <paramref name="id"/> exists.
     /// </summary>
-    /// <param name="id">The Id of the SicCode.</param>
+    /// <param name="id">The ID of the SicCode.</param>
     /// <param name="token"><see cref="T:System.Threading.CancellationToken"/></param>
     /// <returns>True if the SicCode exists; otherwise, false.</returns>
     Task<bool> ExistsAsync(string id, CancellationToken token = default);
@@ -13,7 +13,7 @@ public interface ISicRepository : IDisposable, IAsyncDisposable
     /// <summary>
     /// Returns the <see cref="SicCode"/> with the given <paramref name="id"/>.
     /// </summary>
-    /// <param name="id">The Id of the SicCode.</param>
+    /// <param name="id">The ID of the SicCode.</param>
     /// <param name="token"><see cref="T:System.Threading.CancellationToken"/></param>
     /// <exception cref="EntityNotFoundException{SicCode}">Thrown if no entity exists with the given Id.</exception>
     /// <returns>An SicCode.</returns>
@@ -21,7 +21,7 @@ public interface ISicRepository : IDisposable, IAsyncDisposable
 
     /// <summary>
     /// Returns a read-only collection of all <see cref="SicCode"/> values.
-    /// Returns an empty collection if none exist.
+    /// Returns an empty collection if none exists.
     /// </summary>
     /// <param name="token"><see cref="T:System.Threading.CancellationToken"/></param>
     /// <returns>A read-only collection of SicCode entities.</returns>
