fa48049423a3bb3d608f466b307d5c796f88dc36
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-08-11 11:30:40 -0400

    Migrate to new FluentValidation version (#228)

--- src/WebApp/Platform/PageModelHelpers/ValidationHelper.cs
+++ src/WebApp/Platform/PageModelHelpers/ValidationHelper.cs
@@ -1,5 +1,5 @@
 ﻿using FluentValidation;
-using FluentValidation.AspNetCore;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Runtime.CompilerServices;
 
@@ -15,20 +15,27 @@ public static class ValidationHelper
     /// <param name="modelState">The <see cref="ModelStateDictionary"/> to add any validation errors to.</param>
     /// <param name="dataValue">An optional data value that will be made available to the validator as part of the <see cref="ValidationContext{T}"/>.</param>
     /// <param name="dataKey">The key for the data value.</param>
-    /// <param name="parameterName">The name of the item parameter.</param>
     /// <typeparam name="T">The type of the item to be validated.</typeparam>
     public static async Task ApplyValidationAsync<T>(this IValidator<T> validator, T item,
         ModelStateDictionary modelState, object? dataValue = null,
-        [CallerArgumentExpression(nameof(dataValue))]
-        string? dataKey = null,
-        [CallerArgumentExpression(nameof(item))]
-        string? parameterName = null)
+        [CallerArgumentExpression(nameof(dataValue))] string? dataKey = null)
     {
         var validationResult = dataValue == null || dataKey == null
             ? await validator.ValidateAsync(item)
             : await validator.ValidateAsync(new ValidationContext<T>(item)
                 { RootContextData = { [dataKey] = dataValue } });
 
-        if (!validationResult.IsValid) validationResult.AddToModelState(modelState, parameterName);
+        if (!validationResult.IsValid) validationResult.AddToModelState(modelState);
+    }
+
+    /// <summary>
+    /// Stores the errors in a <see cref="ValidationResult"/> object to the specified <see cref="ModelStateDictionary"/>.
+    /// </summary>
+    /// <param name="result">The ValidationResult to store.</param>
+    /// <param name="modelState">The ModelStateDictionary to store the errors in.</param>
+    private static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState)
+    {
+        foreach (var error in result.Errors)
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
     }
 }
