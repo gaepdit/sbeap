5ddad9b298e66c06c99a008a153734ba90ac4b30
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-10-09 17:39:09 -0400

    Improve app compatibility as an IIS sub-application

--- src/WebApp/Platform/Services/AuthenticationServices.cs
+++ src/WebApp/Platform/Services/AuthenticationServices.cs
@@ -7,7 +7,15 @@ public static class AuthenticationServices
 {
     public static void AddAuthenticationServices(this IServiceCollection services, ConfigurationManager configuration)
     {
-        var authenticationBuilder = services.AddAuthentication();
+        var authenticationBuilder = services
+            .ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = ".Sbeap.Identity";
+                options.Cookie.Path = "/sbeap";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            })
+            .AddAuthentication();
 
         // An Azure AD app must be registered and configured in the settings file.
         if (AppSettings.DevSettings.UseAzureAd)
