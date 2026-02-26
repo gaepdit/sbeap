b6b26ad9cc861b5ee72315abb8843c3f5b48d341
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-02-26 14:53:50 -0500

    Redefine the Customer SIC code database column

---
+++ src/EfRepository/Migrations/20260226195311_ChangeCustomerSicCodeColumn.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sbeap.EfRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCustomerSicCodeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_SicCodes_SicCodeId",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "SicCodeId",
+                table: "Customers",
+                newName: "SicCode");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_SicCodeId",
+                table: "Customers",
+                newName: "IX_Customers_SicCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_SicCodes_SicCode",
+                table: "Customers",
+                column: "SicCode",
+                principalTable: "SicCodes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_SicCodes_SicCode",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "SicCode",
+                table: "Customers",
+                newName: "SicCodeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_SicCode",
+                table: "Customers",
+                newName: "IX_Customers_SicCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_SicCodes_SicCodeId",
+                table: "Customers",
+                column: "SicCodeId",
+                principalTable: "SicCodes",
+                principalColumn: "Id");
+        }
+    }
+}
